2fb47205c3f06f5cd912b61323ed670e6b779a90
sayanbiswas8050-cmd <sayanbiswas8050@gmail.com>
2026-09-13 09:00:24 +0530

    Enhance preprocessing with NLTK downloads and custom stopwords
    Added NLTK downloads for stopwords and WordNet, and defined a custom set of stopwords excluding certain words.

--- preprocessing.py
+++ preprocessing.py
@@ -4,15 +4,20 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
+nltk.download('stopwords', quiet=True)
+nltk.download('wordnet', quiet=True)
+nltk.download('omw-1.4', quiet=True)
+
+lemmatizer = WordNetLemmatizer()
+STOPWORDS = set(stopwords.words("english")) - {'not', 'no', 'yes', 'yet', 'however', 'but'} # exception this stopwords--{'not','no','yes','yet','however','but'}
+
 def preprocess_comment(comment):
     comment = comment.lower().strip()
     # pungtuation
     comment = ' '.join([re.sub("[^A-Za-z0-9\s?!.,|*]",' ',word) for word in comment.split()])
-    # stopwords
-    STOPWORDS = set(stopwords.words("english")) -{'not','no','yes','yet','however','but'}  # exception this stopwords--{'not','no','yes','yet','however','but'}
+    # stopwords  
     comment = ' '.join([word for word in comment.split() if word not in STOPWORDS])
     # Lemmatizer
-    lemmatizer = WordNetLemmatizer()
     comment = ' '.join([lemmatizer.lemmatize(word) for word in comment.split()])
 
     return comment
